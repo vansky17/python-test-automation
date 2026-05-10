981f8ca931e3e49b6930d4ae670c446b7ac4a80b
Ivan Stefanov <50416533+vansky17@users.noreply.github.com>
2026-05-10 16:42:44 +0200

    strings

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-print('My favorite colors are', 'blue', 'green', 'red')
-print(type('blue'))
-print(isinstance('blue', str))
-print(isinstance(7, bool))
-my_str_4 = '''Another another example of a
-multiline and triple quoted
-string'''
-print(my_str_4)
+# print('My favorite colors are', 'blue', 'green', 'red')
+# print(type('blue'))
+# print(isinstance('blue', str))
+# print(isinstance(7, bool))
+# my_str_4 = '''Another another example of a
+# multiline and triple quoted
+# string'''
+# print(my_str_4)
 
-print(len(my_str_4))
-print("line " in my_str_4)
-if "line " in my_str_4:
-    print("Found it!")
+# print(len(my_str_4))
+# print("line " in my_str_4)
+# if "line " in my_str_4:
+#     print("Found it!")
+s = "This is a string"
+print(len(s))
+name = "Johen is"
+age = 26 
+
+print(name + " " + str(age) + " years old")
+
